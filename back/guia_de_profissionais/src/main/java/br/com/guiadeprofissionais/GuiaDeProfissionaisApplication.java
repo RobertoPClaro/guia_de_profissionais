f71d771be2ae84d56cd838c9946736b78eed2580
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-02 21:28:20 -0300

    Criando projeto back em java

---
+++ back/guia_de_profissionais/src/main/java/br/com/guiadeprofissionais/GuiaDeProfissionaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiadeprofissionais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaDeProfissionaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaDeProfissionaisApplication.class, args);
+	}
+
+}
---
+++ back/guia_de_profissionais/src/test/java/br/com/guiadeprofissionais/GuiaDeProfissionaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiadeprofissionais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaDeProfissionaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
